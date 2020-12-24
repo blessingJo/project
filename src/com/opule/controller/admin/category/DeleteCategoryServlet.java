b29f240ed1a890fae371b81db1dcf6c5d494eaf8
Paidamoyo Mangi <pmny@Paidamoyos-MacBook-Pro.local>
2020-12-24 20:07:02 +0000

    implemented delete category feature

---
+++ src/com/opule/controller/admin/category/DeleteCategoryServlet.java
@@ -0,0 +1,30 @@
+package com.opule.controller.admin.category;
+
+import com.opule.controller.admin.BasisServlet;
+import com.opule.service.CategoryServices;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/admin/delete_category")
+public class DeleteCategoryServlet extends BasisServlet {
+	private static final long serialVersionUID = 1L;
+
+    public DeleteCategoryServlet() {
+       
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+	CategoryServices categoryServices = new CategoryServices(entityManager, request, response);
+	categoryServices.deleteCategory();
+	}
+	
+
+}
